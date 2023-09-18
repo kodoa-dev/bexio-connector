cf2336ca4773b96f27688eda386cf712758a2a78
Stéphane Lou <info@aesislabs.com>
2023-09-18 15:13:25 +0200

    Améliorations calculs

--- src/BexioConnector.php
+++ src/BexioConnector.php
@@ -1501,7 +1501,7 @@ class BexioConnector
         return $request->execute();
     }
 
-    public function getBill(int $billId): Response
+    public function getBill(string $billId): Response
     {
         $this->pathParameters['id'] = $billId;
         $request = new ShowBillRequest(...$this->getRequestParameters());
